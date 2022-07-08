e09884f53e39b13d9882588f0ba324f273ea9caa
Ikar <ikar@example.com>
2022-07-08 06:10:52 +0300

    delete the line with original ami id, supposing this was a reason of format failure during action apply

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-#  ami                    = "ami-09e67e426f25ce0d7"
   ami                    = "ami-052efd3df9dad4825"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
